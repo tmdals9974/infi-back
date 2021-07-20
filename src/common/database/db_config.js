7f5fa3690a44de2b2d53ffdd10bbc203083b58ab
SeungMin <tmdals9974@naver.com>
2021-07-20 14:50:07 +0900

    # 2021. 07. 20  - database 정보 변경, dotenv로 환경변수 설정

--- src/common/database/db_config.js
+++ src/common/database/db_config.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 
 let config = {
-    host: "maria-db-01.ciugmf1fx9m6.ap-northeast-2.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "tmdals12!",
-    database: "RestaurantReview",
+    host: process.env.db_host,
+    port: process.env.db_port,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_database,
     connectionLimit: 6
 };
-
 
 module.exports = { config };
